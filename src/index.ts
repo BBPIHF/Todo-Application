587114eaeecd848a6f8be47a0f0e7ccbf0a42cfd
Inaolaji Tope <topeinaolaji@gmail.com>
2023-12-05 18:16:22 +0100

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,66 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from "uuid";
 
-import confetti from 'canvas-confetti';
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.querySelector<HTMLFormElement>("#new-task-form");
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+const clearBtn = document.querySelector<HTMLButtonElement>(".clearBtn");
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+let tasks: User[] = loadTasks();
+tasks.forEach(addListItem);
+
+interface User {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+}
+
+form?.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (input?.value == "" || input?.value == null) return;
+
+  const newTask: User = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+
+  tasks.push(newTask);
+  saveTask();
+
+  addListItem(newTask);
+  input.value = "";
+});
+
+function addListItem(task: User) {
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+    item.classList.toggle("active");
+    saveTask();
+  });
+}
+
+function saveTask() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks));
+}
+
+function loadTasks(): User[] {
+  const taskJSON = localStorage.getItem("TASKS");
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
+}
+
+clearBtn?.addEventListener("click", () => {
+  localStorage.removeItem("TASKS");
+  while (list?.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+});
